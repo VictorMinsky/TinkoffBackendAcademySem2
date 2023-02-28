0c17322202dfc30b5894eff4396961198b70f43f
Viktor Panasyuk <panasyuk.vityu@gmail.com>
2023-02-28 04:00:35 +0300

    fix: GWT pattern in tests

--- handyman/src/test/java/com/tinkoff/handyman/controller/SystemControllerTest.java
+++ handyman/src/test/java/com/tinkoff/handyman/controller/SystemControllerTest.java
@@ -1,45 +1,57 @@
 package com.tinkoff.handyman.controller;
 
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 @AutoConfigureMockMvc
 class SystemControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
 
-    @Test
-    void livenessReturns200() throws Exception {
-        // Given
-        String path = "/system/liveness";
-
-        // When
-        ResultActions response = mockMvc.perform(get(path));
-
-        // Then
-        response.andExpect(status().isOk())
-                .andExpect(jsonPath("$").doesNotExist());
+    @BeforeEach
+    public void initialiseRestAssuredMockMvcWebApplicationContext() {
+        RestAssuredMockMvc.webAppContextSetup(webApplicationContext);
+        RestAssuredMockMvc.mockMvc(mockMvc);
     }
 
     @Test
-    void readinessReturns200AndOK() throws Exception {
-        // Given
+    void livenessReturns200() {
+        String path = "/system/liveness";
+
+        given()
+                .when()
+                .get(path)
+                .then()
+                .statusCode(OK.value())
+                .expect(jsonPath("$").doesNotExist());
+    }
+
+    @Test
+    void readinessReturns200AndOK() {
         String path = "/system/readiness";
 
-        // When
-        ResultActions response = mockMvc.perform(get(path));
-
-        // Then
-        response.andExpect(status().isOk())
-                .andExpect(jsonPath("$.HandymanService").value("OK"));
+        given()
+                .when()
+                .get(path)
+                .then()
+                .statusCode(OK.value())
+                .expect(jsonPath("$.HandymanService").value("OK"));
     }
 }
