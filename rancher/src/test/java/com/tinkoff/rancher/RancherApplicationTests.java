295570c71e08ef902d80acdca3099de72119fc23
Viktor Panasyuk <33966765+VictorMinsky@users.noreply.github.com>
2023-03-06 22:23:10 +0300

    HW2 Done (#2)
    * feat: hw2 done with *
    * feat: ResponseEntity

--- rancher/src/test/java/com/tinkoff/rancher/RancherApplicationTests.java
+++ rancher/src/test/java/com/tinkoff/rancher/RancherApplicationTests.java
@@ -1,13 +1,17 @@
 package com.tinkoff.rancher;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@AutoConfigureMockMvc
 class RancherApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
